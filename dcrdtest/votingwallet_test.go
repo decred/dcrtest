7168b07af1f079b25d0196c480e7b3eb4e6c8a33
Matheus Degiovani <opensource@matheusd.com>
2022-11-16 16:12:38 -0300

    dcrdtest: Enable voting wallet test

--- dcrdtest/votingwallet_test.go
+++ dcrdtest/votingwallet_test.go
@@ -2,10 +2,6 @@
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-// This file is ignored during the regular tests due to the following build tag.
-//go:build rpctest
-// +build rpctest
-
 package rpctest
 
 import (
